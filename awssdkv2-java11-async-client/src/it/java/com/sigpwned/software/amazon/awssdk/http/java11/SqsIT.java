e5e0ba05b17916f4a3288c3f924f9eef24201838
Andy Boothe <andy.boothe@gmail.com>
2024-08-04 21:38:18 -0500

    bump SDK version, improve testcontainers integration

--- awssdkv2-java11-async-client/src/it/java/com/sigpwned/software/amazon/awssdk/http/java11/SqsIT.java
+++ awssdkv2-java11-async-client/src/it/java/com/sigpwned/software/amazon/awssdk/http/java11/SqsIT.java
@@ -1,12 +1,11 @@
 package com.sigpwned.software.amazon.awssdk.http.java11;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.testcontainers.containers.localstack.LocalStackContainer.Service.SQS;
-
 import org.junit.Before;
-import org.junit.Rule;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.containers.localstack.LocalStackContainer.Service;
 import org.testcontainers.utility.DockerImageName;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
@@ -15,20 +14,19 @@ import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 import software.amazon.awssdk.services.sqs.model.ListQueuesResponse;
 
 public class SqsIT {
+  @ClassRule
+  public static LocalStackContainer localstack =
+      new LocalStackContainer(DockerImageName.parse("localstack/localstack:3"))
+          .withServices(Service.SQS);
 
-  DockerImageName localstackImage = DockerImageName.parse("localstack/localstack:3.1.0");
-
-  @Rule
-  public LocalStackContainer localstack = new LocalStackContainer(localstackImage).withServices(
-      SQS);
   public SqsAsyncClient client;
 
   @Before
   public void setupSqsIT() {
     client = SqsAsyncClient.builder().endpointOverride(localstack.getEndpoint())
-        .httpClientBuilder(Java11AsyncHttpClient.builder()).credentialsProvider(
-            StaticCredentialsProvider.create(
-                AwsBasicCredentials.create(localstack.getAccessKey(), localstack.getSecretKey())))
+        .httpClientBuilder(Java11AsyncHttpClient.builder())
+        .credentialsProvider(StaticCredentialsProvider.create(
+            AwsBasicCredentials.create(localstack.getAccessKey(), localstack.getSecretKey())))
         .region(Region.of(localstack.getRegion())).build();
   }
 
